399e144dfc3143d48a01f5adc7ff7c85e9b2427e
bdrupieski <brian.drupieski@gmail.com>
2016-10-19 20:08:35 -0400

    add retweet columns

--- TweetUtils/src/main/sql/anagram/create_tables.sql
+++ TweetUtils/src/main/sql/anagram/create_tables.sql
@@ -8,25 +8,32 @@ CREATE TABLE TWEETS (
   USER_NAME            TEXT      NOT NULL
 );
 
-CREATE INDEX STRIPPED_SORTED_TEXT_INDEX ON TWEETS (STRIPPED_SORTED_TEXT);
+CREATE INDEX STRIPPED_SORTED_TEXT_INDEX
+  ON TWEETS (STRIPPED_SORTED_TEXT);
 
 CREATE TABLE ANAGRAM_MATCHES (
-  ID                                             SERIAL  NOT NULL PRIMARY KEY,
-  TWEET1_ID                                      UUID    NOT NULL REFERENCES TWEETS (ID) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  TWEET2_ID                                      UUID    NOT NULL REFERENCES TWEETS (ID) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  EDIT_DISTANCE_ORIGINAL_TEXT                    INTEGER NOT NULL,
-  EDIT_DISTANCE_STRIPPED_TEXT                    INTEGER NOT NULL,
-  HAMMING_DISTANCE_STRIPPED_TEXT                 INTEGER NOT NULL,
-  LONGEST_COMMON_SUBSTRING_LENGTH_STRIPPED_TEXT  INTEGER NOT NULL,
-  WORD_COUNT_DIFFERENCE                          INTEGER NOT NULL,
-  TOTAL_WORDS                                    INTEGER NOT NULL,
-  INVERSE_LCS_LENGTH_TO_TOTAL_LENGTH_RATIO       REAL    NOT NULL,
-  EDIT_DISTANCE_TO_LENGTH_RATIO                  REAL    NOT NULL,
-  DIFFERENT_WORD_COUNT_TO_TOTAL_WORD_COUNT_RATIO REAL    NOT NULL,
-  IS_SAME_REARRANGED                             INTEGER NOT NULL,
-  INTERESTING_FACTOR                             REAL    NOT NULL,
-  POSTED                                         BOOLEAN NOT NULL,
-  REJECTED                                       BOOLEAN NOT NULL
+  ID                                             SERIAL                              NOT NULL PRIMARY KEY,
+  TWEET1_ID                                      UUID                                NOT NULL REFERENCES TWEETS (ID) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  TWEET2_ID                                      UUID                                NOT NULL REFERENCES TWEETS (ID) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  EDIT_DISTANCE_ORIGINAL_TEXT                    INTEGER                             NOT NULL,
+  EDIT_DISTANCE_STRIPPED_TEXT                    INTEGER                             NOT NULL,
+  HAMMING_DISTANCE_STRIPPED_TEXT                 INTEGER                             NOT NULL,
+  LONGEST_COMMON_SUBSTRING_LENGTH_STRIPPED_TEXT  INTEGER                             NOT NULL,
+  WORD_COUNT_DIFFERENCE                          INTEGER                             NOT NULL,
+  TOTAL_WORDS                                    INTEGER                             NOT NULL,
+  INVERSE_LCS_LENGTH_TO_TOTAL_LENGTH_RATIO       REAL                                NOT NULL,
+  EDIT_DISTANCE_TO_LENGTH_RATIO                  REAL                                NOT NULL,
+  DIFFERENT_WORD_COUNT_TO_TOTAL_WORD_COUNT_RATIO REAL                                NOT NULL,
+  IS_SAME_REARRANGED                             INTEGER                             NOT NULL,
+  INTERESTING_FACTOR                             REAL                                NOT NULL,
+  REJECTED                                       BOOLEAN                             NOT NULL,
+  TWEET1_RETWEET_ID                              BIGINT                              NULL,
+  TWEET2_RETWEET_ID                              BIGINT                              NULL,
+  DATE_RETWEETED                                 TIMESTAMP                           NULL,
+  DATE_CREATED                                   TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
 
-CREATE INDEX INTERESTING_FACTOR_INDEX ON ANAGRAM_MATCHES (INTERESTING_FACTOR);
+CREATE INDEX INTERESTING_FACTOR_INDEX
+  ON ANAGRAM_MATCHES (INTERESTING_FACTOR);
+CREATE INDEX DATE_CREATED_INDEX
+  ON ANAGRAM_MATCHES (date_created);
