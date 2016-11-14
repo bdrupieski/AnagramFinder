58d38501cd78a6e3483097283f2e3e1c1a6a8ca3
bdrupieski <brian.drupieski@gmail.com>
2016-11-14 14:42:16 -0500

    reformat good matches query

--- TweetUtils/src/main/sql/anagram/good_matches.sql
+++ TweetUtils/src/main/sql/anagram/good_matches.sql
@@ -1,46 +1,18 @@
-
--- good matches
-
 SELECT
-  A.INVERSE_LCS_LENGTH_TO_TOTAL_LENGTH_RATIO                            AS LCS_RATIO,
-  A.EDIT_DISTANCE_TO_LENGTH_RATIO                                       AS EDIT_RATIO,
-  A.DIFFERENT_WORD_COUNT_TO_TOTAL_WORD_COUNT_RATIO                      AS COUNT_RATIO,
-  A.IS_SAME_REARRANGED                                                  AS SAME_REARRANGED,
-  A.INTERESTING_FACTOR                                                  AS INTERESTING,
-  T1.ORIGINAL_TEXT                                                      AS T1_ORIG,
-  T2.ORIGINAL_TEXT                                                      AS T2_ORIG,
-  CONCAT('http://twitter.com/', T1.USER_NAME, '/status/', T1.STATUS_ID) AS TWEET1_URL,
-  CONCAT('http://twitter.com/', T2.USER_NAME, '/status/', T2.STATUS_ID) AS TWEET2_URL,
-  T1.CREATED_AT                                                         AS T1_CREATED,
-  T2.CREATED_AT                                                         AS T2_CREATED
+  anagram_matches.inverse_lcs_length_to_total_length_ratio                      AS lcs_ratio,
+  anagram_matches.edit_distance_original_text                                   AS edit_ratio,
+  anagram_matches.different_word_count_to_total_word_count_ratio                AS diff_word_count_ratio,
+  anagram_matches.is_same_rearranged                                            AS same_rearranged,
+  anagram_matches.interesting_factor                                            AS interesting,
+  tweet1.original_text                                                          AS t1_text,
+  tweet2.original_text                                                          AS t2_text,
+  CONCAT('http://twitter.com/', tweet1.user_name, '/status/', tweet1.status_id) AS t1_url,
+  CONCAT('http://twitter.com/', tweet2.user_name, '/status/', tweet2.status_id) AS t2_url,
+  tweet1.created_at                                                             AS t1_created,
+  tweet2.created_at                                                             AS t2_created
 FROM
-  ANAGRAM_MATCHES A
-  INNER JOIN TWEETS T1 ON A.TWEET1_ID = T1.ID
-  INNER JOIN TWEETS T2 ON A.TWEET2_ID = T2.ID
+  anagram_matches
+  INNER JOIN tweets tweet1 ON anagram_matches.tweet1_id = tweet1.id
+  INNER JOIN tweets tweet2 ON anagram_matches.tweet2_id = tweet1.id
 ORDER BY
-  A.INTERESTING_FACTOR DESC;
-
--- recent good matches
-
-SELECT
-  A.INVERSE_LCS_LENGTH_TO_TOTAL_LENGTH_RATIO                            AS LCS_RATIO,
-  A.EDIT_DISTANCE_TO_LENGTH_RATIO                                       AS EDIT_RATIO,
-  A.DIFFERENT_WORD_COUNT_TO_TOTAL_WORD_COUNT_RATIO                      AS COUNT_RATIO,
-  A.IS_SAME_REARRANGED                                                  AS SAME_REARRANGED,
-  A.INTERESTING_FACTOR                                                  AS INTERESTING,
-  T1.ORIGINAL_TEXT                                                      AS T1_ORIG,
-  T2.ORIGINAL_TEXT                                                      AS T2_ORIG,
-  CONCAT('http://twitter.com/', T1.USER_NAME, '/status/', T1.STATUS_ID) AS TWEET1_URL,
-  CONCAT('http://twitter.com/', T2.USER_NAME, '/status/', T2.STATUS_ID) AS TWEET2_URL,
-  T1.CREATED_AT                                                         AS T1_CREATED,
-  T2.CREATED_AT                                                         AS T2_CREATED
-FROM
-  anagram_matches A
-  INNER JOIN TWEETS T1 ON A.TWEET1_ID = T1.ID
-  INNER JOIN TWEETS T2 ON A.TWEET2_ID = T2.ID
-WHERE
-  A.inverse_lcs_length_to_total_length_ratio > 0.4 AND
-  A.different_word_count_to_total_word_count_ratio > 0.4 AND
-  A.edit_distance_to_length_ratio > 0.4
-ORDER BY
-  A.date_created desc;
+  anagram_matches.interesting_factor DESC;
